1060376914a7f97bc804bd6e49beb8e27093944c
Otor John Stephen <johnogenyi4real@gmail.com>
2021-02-05 12:53:54 +0100

    added contents and styles to the why choose use section

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import photo from '../asset/images/bg.jpg';
 import heartrate from '../asset/images/heartrate.png';
+import stethoscope from '../asset/images/doc.png';
 
 
 
@@ -12,8 +13,8 @@ const Home = () => {
                 <div className="home-data">
                     <div className="home-title">take care of your <br /> <span>Health</span></div>
                     <p className="home-description">Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                        Incidunt assumenda nemo, quod corrupti soluta veniam voluptatibus praesentium voluptate
-                        recusandae perspiciatis rerum aspernatur suscipit sunt iusto voluptates. Sit ducimus ipsa hic.
+                    Incidunt assumenda nemo, quod corrupti soluta veniam voluptatibus praesentium voluptate
+                    recusandae perspiciatis rerum aspernatur suscipit sunt iusto voluptates. Sit ducimus ipsa hic.
                     </p>
                     <Link to='/' className="button">View More</Link>
                 </div>
@@ -36,6 +37,54 @@ const Home = () => {
                             <li>We provide high patient confidentiality</li>
                             <li>We provide high-quality services for the whole family</li>
                         </ul>
+                    </div>
+                </div>
+                <div className="reason-section container-fluid">
+                    <div className="reason row">
+                        <div className="text col-md-6">
+                            <div className="text-title">why choose us</div>
+                            <img src={heartrate} alt="heartrate image" className="heartrate" />
+                            <div className="divs row">
+                                <div className="first col-md-6">
+                                    <div className="title">qualified doctors</div>
+                                    <div className="description">
+                                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Non rerum quia doloribus 
+                                        corporis ad minus, dolore, ullam, officiis commodi odio sunt odit? Aliquid nesciunt 
+                                        natus optio atque, alias itaque possimus.
+                                    </div>
+                                </div>
+
+                                <div className="first col-md-6">
+                                    <div className="title">emergency care</div>
+                                    <div className="description">
+                                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Non rerum quia doloribus 
+                                        corporis ad minus, dolore, ullam, officiis commodi odio sunt odit? Aliquid nesciunt 
+                                        natus optio atque, alias itaque possimus.
+                                    </div>
+                                </div>
+
+                                <div className="first col-md-6">
+                                    <div className="title">24 hour services</div>
+                                    <div className="description">
+                                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Non rerum quia doloribus 
+                                        corporis ad minus, dolore, ullam, officiis commodi odio sunt odit? Aliquid nesciunt 
+                                        natus optio atque, alias itaque possimus.
+                                    </div>
+                                </div>
+
+                                <div className="first col-md-6">
+                                    <div className="title">operation theater</div>
+                                    <div className="description">
+                                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Non rerum quia doloribus 
+                                        corporis ad minus, dolore, ullam, officiis commodi odio sunt odit? Aliquid nesciunt 
+                                        natus optio atque, alias itaque possimus.
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="image col-md-4">
+                            <img src={stethoscope} alt="" className="stethoscope"/>
+                        </div>
                     </div>
                 </div>
             </div>
